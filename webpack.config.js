d502c36b9ba4401b7fdaab6badef07b7c739b1e0
Carlos Dominguez <sahrlybip/@gmail.com>
2023-03-21 23:37:44 -0600

    variables de entorno

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,76 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
+
+const ruleForJs = {
+    test: /\.m?js/,
+    exclude: /node_modules/,
+    use: {
+        loader: 'babel-loader'
+    }
+}
+
+const ruleForCss = {
+    test:/\.css|.styl$/i,
+    use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'stylus-loader'
+    ]
+}
+const ruleForPng= {
+    test:/\.png/,
+    type: 'asset/resource'
+}
+const ruleForFonts = {
+    test: /\.(woff|woff2)$/,
+    use: {
+        loader: 'url-loader',
+        options: {
+            limit: 10000,
+            mimetype: "application/font=woff",
+            name: "[name].[contenthash].[ext]",
+            outputPath: './assets/fonts/',
+            publicPath: './fonts/',
+            esModule: false
+            
+        }
+    }
+}
+
+
+const rules = [ruleForJs, ruleForCss, ruleForPng, ruleForFonts]
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename : 'main.js'
+        filename : '[name].[contenthash].js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     resolve: {
-        extensions: [
-            '.js',
-        ]
+        extensions: ['.js'],
+        alias: {
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images/'),
+        }
     },
-    module: {
-        rules: [
-            {
-                test: /\.m?js/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test:/\.css|.styl$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'stylus-loader'
-                ]
-            }
-        ]
-    },
+    module: { rules  },
     plugins: [
         new HtmlWebpackPlugin({
             inject: true,
             template: './public/index.html',
             filename: './index.html'
         }),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: './assets/[name].[contenthash].css'
+        }),
         new CopyPlugin({
             patterns: [
                 {
@@ -48,6 +79,14 @@ module.exports = {
                     to: "assets/images"
                 }
             ]
-        })
-    ]
+        }),
+        new Dotenv(),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ],
+    },
 }
